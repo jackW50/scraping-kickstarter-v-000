b7b2e7c20b227632bf371b5cba3fa16cabedc53d
John Werenski <jtwerenski@gmail.com>
2018-10-06 17:59:41 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -10,7 +10,7 @@ def create_project_hash
   kickstarter.css("li.project.grid_4").each do |p|
     title = p.css("h2.bbcard_name strong a").text.to_sym
     projects[title] = {} 
-    projects[title][:image_link] = p.css("div.project_thumbnail a img").attribute("src").value 
+    projects[title][:image_link] = p.css("div.project-thumbnail a img").attribute("src").value 
     projects[title][:description] = p.css("p.bbcard_blurb").text
     projects[title][:location] = p.css("ul.project-meta li span.location-name").text 
     projects[title][:percent_funded] = p.css("ul.project_stats li.first.funded strong").text.gsub("%", "").to_i 
