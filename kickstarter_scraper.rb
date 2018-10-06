7745ed41717206edf8735bc3689c31204b4a3b2c
John Werenski <jtwerenski@gmail.com>
2018-10-06 17:30:03 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -14,4 +14,5 @@ end
     #image_link = p.css("div.project_thumbnail a img").attribute("src").value  
     #description = p.css("p.bbcard_blurb").text 
     #location = p.css("ul.project-meta li a span.location-name").text 
+    #percent funded = p.css("ul.project-stats li.first.funded strong").text 
 create_project_hash
