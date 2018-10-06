b24d24a4b7cd3c269677709aee9812b8cbf82e15
John Werenski <jtwerenski@gmail.com>
2018-10-06 17:16:34 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -11,5 +11,7 @@ end
 # projects: kickstarter.css("li.project.grid_4")
 # projects each do |p|
     #title = p.css("h2.bbcard_name strong a")
-    #image_link = p.("div.project_thumbnail a img").attribute("src").value  
+    #image_link = p.css("div.project_thumbnail a img").attribute("src").value  
+    #description = p.css("p.bbcard_blurb").text 
+    
 create_project_hash
