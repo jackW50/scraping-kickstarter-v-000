a920ad50d784bd9a5d91f18bbb9e6a7a39d3eafb
John Werenski <jtwerenski@gmail.com>
2018-10-06 18:00:09 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -25,7 +25,7 @@ end
 # projects: kickstarter.css("li.project.grid_4").text 
 # projects each do |p|
     #title = p.css("h2.bbcard_name strong a")
-    #image_link = p.css("div.project_thumbnail a img").attribute("src").value  
+    #image_link = p.css("div.project-thumbnail a img").attribute("src").value  
     #description = p.css("p.bbcard_blurb").text 
     #location = p.css("ul.project-meta li a span.location-name").text 
     #percent funded = p.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
